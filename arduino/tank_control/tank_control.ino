4c369e162dc34f848d7886479cb0c05e6ad0255a
jrothfeder <rothbutter@google.com>
2019-12-03 12:58:34 -0500

    Add arduino code to control the tank.

---
+++ arduino/tank_control/tank_control.ino
@@ -0,0 +1,121 @@
+#include <Servo.h>
+
+int E1 = 6; //M1 Speed Control
+int E2 = 5; //M2 Speed Control
+int M1 = 8; //M1 Direction Control
+int M2 = 7; //M2 Direction Control
+
+#define MIN_ANGLE 100
+#define MAX_ANGLE 180
+#define SERVO_PIN 13
+
+Servo servo;
+int currentServoAngle = 100;
+
+void setup(void)
+{
+  int i;
+  for(i=5;i<=8;i++)
+  pinMode(i, OUTPUT);
+  
+  servo.attach(SERVO_PIN);
+  servo.write(currentServoAngle);
+      
+  Serial.begin(9600);
+  Serial.println("setup");
+}
+
+void loop(void)
+{
+  if (Serial.available() > 0) {
+    handleSerial(Serial.read());
+  }
+}
+
+void stop(void) //Stop
+{
+  digitalWrite(E1,LOW);
+  digitalWrite(E2,LOW);
+  Serial.println("stop");
+}
+
+void forward(char a,char b)
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,LOW);
+  analogWrite (E2,b);
+  digitalWrite(M2,LOW);
+  Serial.println("forward");
+}
+
+void reverse (char a,char b)
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,HIGH);
+  analogWrite (E2,b);
+  digitalWrite(M2,HIGH);
+  Serial.println("reverse");
+}
+
+void left (char a,char b)
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,HIGH);
+  analogWrite (E2,b);
+  digitalWrite(M2,LOW);
+  Serial.println("left");
+}
+
+void right (char a,char b)
+{
+  analogWrite (E1,a);
+  digitalWrite(M1,LOW);
+  analogWrite (E2,b);
+  digitalWrite(M2,HIGH);
+  Serial.println("right");
+}
+
+void moveServoUp(int amount) {
+  currentServoAngle = min(MAX_ANGLE, currentServoAngle + amount);
+  servo.write(currentServoAngle);
+}
+
+void moveServoDown(int amount) {
+  currentServoAngle = max(MIN_ANGLE, currentServoAngle - amount);
+  servo.write(currentServoAngle);
+}
+
+void handleSerial(char val) {
+  int leftspeed = 255; //255 is maximum speed
+  int rightspeed = 240;
+  switch(val) // Perform an action depending on the command
+  {
+    case 'w'://Move Forward
+    case 'W':
+      forward (leftspeed,rightspeed);
+      break;
+    case 's'://Move Backwards
+    case 'S':
+      reverse (leftspeed,rightspeed);
+      break;
+    case 'a'://Turn Left
+    case 'A':
+      left (leftspeed,rightspeed);
+      break;
+    case 'd'://Turn Right
+    case 'D':
+      right (leftspeed,rightspeed);
+      break;
+    case 'r': //Tilt UP
+    case 'R':
+      moveServoUp(1);
+      break;
+    case 'f':
+    case 'F':
+      moveServoDown(1);
+      break;
+    default:
+      stop();
+      break;
+  }
+}
